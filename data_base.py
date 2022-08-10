3ef47a4007b4a4268b7ba119565aa83287cef686
STONE17th <17th@inbox.ru>
2022-08-10 10:27:09 +0300

    Рабочая версия бота с MySQL

--- data_base.py
+++ data_base.py
@@ -1,25 +1,67 @@
-import sqlite3 as sqdb
+import mysql.connector as sqdb
 
 
 class DataBase():
-    def __init__(self, path=None):
-        self.path = path
-        self.base = sqdb.connect(self.path)
+    def __init__(self, base):
+        self.base = base
         self.cur = self.base.cursor()
-        self.create_table()
+        # self.create_table('user_list')
+        # self.create_table('quest_list')
 
-    def create_table(self):
-        self.base.execute('CREATE TABLE IF NOT EXISTS user_list (id_user PRIMARY KEY, name, task)')
+    def create_table(self, list_select: str):
+        match list_select:
+            case 'user_list':
+                self.base.execute(f'CREATE TABLE IF NOT EXISTS {list_select} (id_user PRIMARY KEY, name, task)')
+            case 'quest_list':
+                self.base.execute(f'CREATE TABLE IF NOT EXISTS {list_select} (id_quest PRIMARY KEY, task, answer)')
         self.base.commit()
 
-    def get_data(self, user_id):
-        res = self.cur.execute(f'SELECT id_user, name, task FROM user_list WHERE id_user = {user_id}').fetchall()
-        return res
+    def get_user(self, list_select: str, *args):
+        match list_select:
+            case 'user_list':
+                self.cur.execute(f'SELECT dis_user_id, name, task FROM user_list WHERE dis_user_id = {args[0]}')
+                return [tuple(elem) for elem in self.cur]
+            case 'user_task':
+                self.cur.execute(f'SELECT task FROM user_list WHERE dis_user_id = {args[0]}')
+                return [elem[0] for elem in self.cur]
 
-    def add_item(self, new_item):
-        self.cur.execute('INSERT INTO user_list (id_user, name, task) VALUES (?, ?, ?)', new_item)
+
+    def get_quest(self, list_select: str, *args):
+        match list_select:
+            case 'quest_list':
+                self.cur.execute(f'SELECT * FROM quest_list')
+                return [tuple(elem) for elem in self.cur]
+            case 'quest_id':
+                self.cur.execute(f'SELECT quest_id FROM quest_list')
+                return [elem[0] for elem in self.cur]
+            case 'quest_list_new':
+                self.cur.execute(f'SELECT quest_id, task, answer FROM quest_list WHERE task = {args[0]}').fetchall()
+            case 'quest_task':
+                self.cur.execute(f'SELECT task FROM quest_list WHERE quest_id = {args[0]}')
+                return [elem[0] for elem in self.cur]
+            case 'quest_answer':
+                self.cur.execute(f'SELECT answer FROM quest_list WHERE quest_id = {args[0]}')
+                return [elem for elem in self.cur][0]
+
+
+    def add_item(self, new_item, list_select: str):
+        match list_select:
+            case 'user_list':
+                self.cur.execute(f'INSERT INTO {list_select} (dis_user_id, name, task) VALUES (%s, %s, %s)', new_item)
+            case 'quest_list':
+                self.cur.execute(f'INSERT INTO {list_select} (task, answer) VALUES (?, ?)', new_item)
         self.base.commit()
 
-    def update_item(self, task, user_id):
-        self.cur.execute(f'UPDATE user_list SET task = {task} WHERE id_user = {user_id}')
+    def update_item(self, task, user_id, list_select: str):
+        match list_select:
+            case 'user_list':
+                self.cur.execute(f'UPDATE {list_select} SET task = {task} WHERE id_user = {user_id}')
+        self.base.commit()
+
+    def delete_item(self, id, list_select: str):
+        match list_select:
+            case 'user_list':
+                self.cur.execute(f'DELETE FROM {list_select} WHERE id_user = {id}')
+            case 'quest_list':
+                self.cur.execute(f'DELETE FROM {list_select} WHERE id_quest = {id}')
         self.base.commit()
--- main.py
+++ main.py
@@ -1,68 +1,103 @@
-import sqlite3
-from random import randint
+import asyncio
+import random
 import discord
+import mysql.connector
 from discord.ext import commands
 import os
 import data_base
 
-bot = commands.Bot(command_prefix='!', Intents=discord.Intents.all)
-bot.remove_command('help')
-user = data_base.DataBase('disbot.db')
-simple_role = 'GeekBrains'
+bot = commands.Bot(command_prefix='/', intents=discord.Intents.all())
 
-quest = {
-    1: 'a = "Python"\nb = "Love"\nres = (a + b) * 2\nprint(res)\n\n перед ответом напишите команду !answer',
-    2: 'a = 5 // 2\nb = 5 % 2\nprint(a > b)\n\n перед ответом напишите команду !answer',
-    3: 'что будет выведено в консоль в результате кода:\na = 1\nb = a + 4\na = b\nprint(a+b)\n\n перед ответом напишите команду !answer'
+base = quests = quests_id = None
 
-}
-quest_answers={
-    1: 'PythonLovePythonLove',
-    2: 'True',
-    3: '10'
-}
+one_level_role = 1006203836463071325
+guild_id = 761885493394407435
+start_channel = 1006350953848512605
 
+status = '/info'
+task_string = f'что выведет в консоль этот код:\n'
+answer_string = f'\n\nОтвет отправляй так: /answer <твой вариант ответа>'
 
 @bot.event
 async def on_ready():
+    global quests, quests_id
     print('On start')
+    dbase = data_base.DataBase(
+        mysql.connector.connect(user='root', db='cf_bot', passwd='CodeFather17', host='glt.ekolenko.ru'))
+    if dbase:
+        print('DB Connected... OK')
+        quests = dbase.get_quest('quest_list')
+        quests_id = dbase.get_quest('quest_id')
 
-    global base, cur
-    base = sqlite3.connect('disbot.db')
-    cur = base.cursor()
-    if base: print('DB connected')
+# [print(task[1]) for task in quests]
+# print(random.choice(quests_id))
 
+@bot.event
+async def on_member_join(member):
+    welcome = bot.get_channel(start_channel)
+    embed=discord.Embed(title="Добро пожаловать!", description=f'Эй, народ! Зацените! Кто это тут к нам залетел :)\n\nПривет, {member.mention}. Я бот канала CODE Father\'s. Пока я мало чего умею, но всё впереди...\n\nЗагляни в ЛС, я там тебе кое-чего прислал', color=0xCC974F)
+    await welcome.send(embed=embed)
+    await member.send(f'Привет, {member.name}! Рады приветствовать тебя на нашем сервере. Чем мы здесь занимаемся?\nМы создаем дружное коммьюнити из единомышленников в IT сфере. Здесь ты сможешь получить помощь с ДЗ, получить консультацию по текущим темам от однокурсников, пообщаться в прямом эфире с крутыми гостями, которые уже работают в IT, обменяться опытом, найти команду для реализации своих идей, да и просто пообщаться :)\nЕсли возникнут вопросы, то пиши кому-нибудь из администараторов и тебе обязательно ответят\n\nНо для начала было бы неплохо получить роль первого уровня (для доступа к голосовому чату и архиву с полезными ссылками)\nДля этого просто введи на канале /access и мы с тобой всё сделаем!\n\nПриятных тебе минут на сервере и удачного обучения!\n\nP.S. Если увидишь Джонна Конора - передай привет')
+    new_user(member)
+
+@bot.event
+async def on_member_remove(member):
+    dbase.delete_item(member.id, 'user_list')
+
+def new_user(member):
+    global quests_id
+    task = random.choice(quests_id)
+    new_user = ((str(member.id), member.name, task))
+    dbase.add_item(new_user, 'user_list')
+
+async def delete_message(ctx):
+    try:
+        await ctx.message.delete()
+    except:
+        print('Ошибка')
 
 @bot.command()
-async def help(ctx):
-    await ctx.send(f'{ctx.author.mention}, для получения роли GeekBrains (доступ к голосовому каналу и дополнительным материалам) отправьте боту команду !access')
+async def info(ctx):
+    await ctx.author.send(f'{ctx.author.mention}, для получения роли GeekBrains (доступ к голосовому каналу и дополнительным материалам) отправьте боту команду /access')
 
 @bot.command()
 async def access(ctx, *args):
-    global user, quest
-    if user.get_data(ctx.author.id) and not user.get_data(ctx.author.id)[0][2] == 0:
-        cur_user = user.get_data(ctx.author.id)
-        task = cur_user[0][2]
-        await ctx.send(f"{ctx.author.mention}, {quest.get(task)}")
+    global dbase, one_level_role
+    guild = bot.get_guild(guild_id)
+    member = guild.get_member(ctx.message.author.id)
+    if not dbase.get_user('user_list', ctx.author.id):
+        new_user(ctx.author)
+    for role in member.roles:
+        if one_level_role == role.id:
+            await ctx.send(f"У вас уже есть такая роль")
+            break
     else:
-        new_user = ((ctx.author.id, ctx.author.name, 0))
-        user.add_item(new_user)
-        task = randint(1,len(quest))
-        await ctx.send(f"{ctx.author.mention}, {quest.get(task)}")
-        user.update_item(task, ctx.author.id)
+        cur_user = dbase.get_user('user_list', ctx.author.id)
+        cur_quest = dbase.get_quest('quest_task', cur_user[0][2])
+        await ctx.send(f'{ctx.author.mention}, {task_string}{cur_quest[0]}{answer_string}')
 
 @bot.command()
 async def answer(ctx, *args):
-    global user, quest_answers, simple_role
+    global dbase, quest_answers, simple_role
     if not args == ():
-        role = discord.utils.get(ctx.author.guild.roles, name=simple_role)
-        if args[0] == quest_answers.get(user.get_data(ctx.author.id)[0][2]):
-            await ctx.message.delete()
-            await ctx.author.add_roles(role)
-            await ctx.send(f"{ctx.author.mention}, роль выдана!")
+        guild = bot.get_guild(guild_id)
+        role = guild.get_role(one_level_role)
+        member = guild.get_member(ctx.message.author.id)
+        task_id = dbase.get_user('user_task', ctx.author.id)
+        if args[0] == dbase.get_quest('quest_answer', task_id[0])[0]:
+            await delete_message(ctx)
+            await member.add_roles(role)
+            await ctx.send(f"{ctx.author.mention}, поздравляем! Теперь у тебя роль первого уровня!")
         else:
-            await ctx.message.delete()
-            await ctx.send(f"{ctx.author.mention}, ну чего-то ты не то написал. Разберись для начала с этим, а потом уже роль")
+            await delete_message(ctx)
+            await ctx.send(f'{ctx.author.mention}, ну чего-то ты не то написал. Разберись для начала с этим, а потом уже роль')
 
+async def game_info():
+    global status
+    await bot.wait_until_ready()
+    while not bot.is_closed():
+        await bot.change_presence(activity=discord.Game(status))
+        await asyncio.sleep(15)
+
+bot.loop.create_task(game_info())
 bot.run(os.getenv('TOKEN'))
-
