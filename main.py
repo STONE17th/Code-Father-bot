5d0fa61d767b6e41bbb4215c1e11b64ae1a62124
STONE17th <17th@inbox.ru>
2022-08-20 17:03:07 +0300

    Обновлен функционал

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ async def delete_message(ctx):
 
 @bot.command()
 async def info(ctx):
-    # print(dir(ctx.guild.get_role))
+    await delete_message(ctx)
     member_roles = [role.id for role in bot.get_guild(guild_id).get_member(ctx.message.author.id).roles if role.mentionable]
     print(cf_role.get(1))
     for role in member_roles:
@@ -97,7 +97,7 @@ async def info(ctx):
                 pass
             case 4:
                 print('Четвертый')
-                await ctx.author.send(f'{ctx.author.mention}, у вас статус admin и вы можете:\n/embed <Заголовок> <Текст сообщения> - Загловок из одного слова, текст сообщения - сколько угодно\n/set_task <пользователь> <номер задачи> - выдать пользователю новую задачу, пользователя можно задать кликнув по нему правой кнопкой и выбрать Упомянуть\n/status <пользователь> <должность> - выдать пользователю должность, упомянуть пользователя удобно правой кнопкой из должностей пока только admin и user')
+                # await ctx.author.send(f'{ctx.author.mention}, у вас статус admin и вы можете:\n/embed <Заголовок> <Текст сообщения> - Загловок из одного слова, текст сообщения - сколько угодно\n/set_task <пользователь> <номер задачи> - выдать пользователю новую задачу, пользователя можно задать кликнув по нему правой кнопкой и выбрать Упомянуть\n/status <пользователь> <должность> - выдать пользователю должность, упомянуть пользователя удобно правой кнопкой из должностей пока только admin и user')
     # await ctx.author.send(f'{ctx.author.mention}, для получения роли первого уровня (доступ к голосовому каналу и дополнительным материалам) отправьте боту команду /access')
 
 @bot.command()
