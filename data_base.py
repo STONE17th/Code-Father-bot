634921956c654efd18fd945f2cf43f38f37182e8
STONE17th <17th@inbox.ru>
2022-08-20 16:19:55 +0300

    Обновлен функционал

--- data_base.py
+++ data_base.py
@@ -11,7 +11,7 @@ class DataBase():
     def create_table(self, list_select: str):
         match list_select:
             case 'user_list':
-                self.base.execute(f'CREATE TABLE IF NOT EXISTS {list_select} (user_id PRIMARY KEY, dis_user_id, name, user_status, task)')
+                self.base.execute(f'CREATE TABLE IF NOT EXISTS {list_select} (user_id PRIMARY KEY, dis_id, name, task, points, copa)')
             case 'quest_list':
                 self.base.execute(f'CREATE TABLE IF NOT EXISTS {list_select} (quest_id PRIMARY KEY, task, answer)')
         self.base.commit()
@@ -19,19 +19,19 @@ class DataBase():
     def get_user(self, list_select: str, *args):
         match list_select:
             case 'user':
-                self.cur.execute(f'SELECT dis_user_id, name, task FROM user_list WHERE dis_user_id = {args[0]}')
+                self.cur.execute(f'SELECT dis_id, name, task FROM user_list WHERE dis_id = {args[0]}')
                 return [tuple(elem) for elem in self.cur]
             case 'task':
-                self.cur.execute(f'SELECT task FROM user_list WHERE dis_user_id = {args[0]}')
+                self.cur.execute(f'SELECT task FROM user_list WHERE dis_id = {args[0]}')
                 return [elem[0] for elem in self.cur]
             case 'status':
-                self.cur.execute(f'SELECT user_status FROM user_list WHERE dis_user_id = {args[0]}')
+                self.cur.execute(f'SELECT status FROM user_list WHERE dis_id = {args[0]}')
                 return [elem[0] for elem in self.cur]
 
 
     def get_quest(self, list_select: str, *args):
         match list_select:
-            case 'list':
+            case 'all':
                 self.cur.execute(f'SELECT * FROM quest_list')
                 return [tuple(elem) for elem in self.cur]
             case 'id':
@@ -45,30 +45,24 @@ class DataBase():
             case 'answer':
                 self.cur.execute(f'SELECT answer FROM quest_list WHERE quest_id = {args[0]}')
                 return [elem for elem in self.cur][0]
-            case 'set_quest':
-                self.cur.execute(f'UPDATE user_list SET task = {args[1]} WHERE dis_user_id = {args[0]}')
-                return [elem for elem in self.cur][0]
 
-    def add_item(self, new_item, list_select: str):
+    def add_item(self, list_select: str, new_item):
         match list_select:
             case 'new_user':
-                self.cur.execute(f'INSERT INTO user_list (dis_user_id, name, task, user_status) VALUES (%s, %s, %s, %s)', new_item)
+                self.cur.execute(f'INSERT INTO user_list (dis_id, name, task, points, copa) VALUES (%s, %s, %s, %s, %s)', new_item)
             case 'new_quest':
                 self.cur.execute(f'INSERT INTO quest_list (task, answer) VALUES (%s, %s)', new_item)
         self.base.commit()
 
-    def update_item(self, choice, dis_user_id, num):
-        match choice:
+    def update_item(self, list_select: str, dis_user_id, num):
+        match list_select:
             case 'set_task':
-                self.cur.execute(f'UPDATE user_list SET task = %s WHERE dis_user_id = %s', (num, dis_user_id))
-            case 'set_status':
-                self.cur.execute(f'UPDATE user_list SET user_status = %s WHERE dis_user_id = %s', (num, dis_user_id))
-        self.base.commit()
+                self.cur.execute(f'UPDATE user_list SET task = %s WHERE dis_id = %s', (num, dis_user_id))
 
     def delete_item(self, id, list_select: str):
         match list_select:
             case 'user_list':
-                self.cur.execute(f'DELETE FROM {list_select} WHERE dis_user_id = {id}')
+                self.cur.execute(f'DELETE FROM {list_select} WHERE dis_id = {id}')
             case 'quest_list':
                 self.cur.execute(f'DELETE FROM {list_select} WHERE quest_id = {id}')
         self.base.commit()
